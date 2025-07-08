964f22d33561f6c182f81bcbdf56181e658177a9
CanvasofSpores <emmajadecoffin@gmail.com>
2025-07-08 13:45:49 -0400

    Move from p10k to Oh-My-Zsh+p10k

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+opts = {
+  adapters = {
+    ["neotest-python"] = {
+      -- Here you can specify the settings for the adapter, i.e.
+      runner = "pytest",
+      -- python = ".venv/bin/python",
+    },
+  },
+}
