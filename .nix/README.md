5b559f8077460b89532b0085d652b7446c803557
CanvasofSpores <emmajadecoffin@gmail.com>
2025-08-22 21:52:30 -0400

    Initial NixOS Config commit

---
+++ .nix/README.md
@@ -0,0 +1,3 @@
+# Personal Nix Configs
+
+This is for my personal flake-based NixOS and Home-Manager configs.
---
+++ .nix/home.nix
@@ -0,0 +1,129 @@
+{ config, pkgs, catppuccin, spicetify-nix, spicepkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "canvas";
+  home.homeDirectory = "/home/canvas";
+  nix.package = pkgs.nix;
+  nixpkgs.config.allowUnfree = true;
+ 
+  
+  # Catppuccin configs.
+  catppuccin = {
+    enable = true;
+    flavor = "mocha";
+    accent = "lavender";
+#    grub = { enable = true; };
+#    sddm = { enable = true; };
+#    tty = { enable = true; };
+#    btop = { enable = true; };
+#    firefox = { enable = true; };
+#    hyprland = { enable = true; };
+#    cursors = { enable = true; };
+#    gtk = { enable = true; };
+#    qt = { enable = true; };
+#    kvantum = { enable = true; };
+#    kitty = { enable = true; };
+#    mangohud = { enable = true; };
+#    nvim = { enable = true; };
+#    swaylock = { enable = true; };
+#    swaync = { enable = true; };
+#    zsh-syntax-highlighting = { enable = true; };
+
+  };
+
+  programs.spicetify = {
+    enable = true;
+    #config options
+    theme = spicepkgs.themes.dribbblishDynamic;
+    enabledExtensions = with spicepkgs.extensions; [
+      shuffle
+      playingSource
+      beautifulLyrics
+      addToQueueTop
+      queueTime
+    ];
+    enabledCustomApps = with spicepkgs.apps; [
+     lyricsPlus
+     historyInSidebar
+    ];
+    enabledSnippets = with spicepkgs.snippets; [
+      modernScrollbar
+      hideDownloadButton
+      rotatingCoverart
+      spinningCdCoverArt
+      removeTheArtistsAndCreditsSectionsFromTheSidebar
+    ];
+    windowManagerPatch = true;
+  };
+
+
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+
+  };
+
+
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/canvas/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  home.stateVersion = "25.05"; # This is the version that this Config was created at. DO NOT CHANGE.
+  }
