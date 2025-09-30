a3adacf7c6c7ef48e397a6f13c578ef88e64fddc
CanvasofSpores <emmajadecoffin@gmail.com>
2025-09-30 16:38:35 -0400

    funny prompt fixes

---
+++ .config/mommy/config.sh
@@ -0,0 +1,70 @@
+## Configuration
+MOMMY_CAREGIVER="mommy/mama"
+MOMMY_PRONOUNS="she her her hers herself/we us our ours ourselves"
+MOMMY_SWEETIE="canvas/songbird/girl/pet/petal"
+MOMMY_PREFIX=""
+MOMMY_SUFFIX="~"
+
+MOMMY_CAPITALIZE="0"
+MOMMY_COLOR="005"
+
+MOMMY_COMPLIMENTS="
+# generic~
+*pets your head*
+amazing work as always
+
+# good X~
+good %%SWEETIE%%
+good job, %%SWEETIE%%
+that's a good %%SWEETIE%%
+who's my good %%SWEETIE%%
+good floret
+that's a good floret
+who's my good floret
+
+# proud~
+%%CAREGIVER%% is very proud of you
+%%CAREGIVER%% is so proud of you
+%%CAREGIVER%% knew you could do it
+%%CAREGIVER%% loves you, you are doing amazing
+
+# compliment~
+%%CAREGIVER%%'s %%SWEETIE%% is so smart
+
+# reward~
+%%CAREGIVER%% thinks you deserve a special treat for that
+my little %%SWEETIE%% deserves a big fat kiss for that
+"
+MOMMY_COMPLIMENTS_EXTRA=""
+MOMMY_COMPLIMENTS_ENABLED="1"
+
+MOMMY_ENCOURAGEMENTS="
+# trust~
+%%CAREGIVER%% believes in you
+%%CAREGIVER%% knows you'll get there
+%%CAREGIVER%% knows %%THEIR%% little %%SWEETIE%% can do better
+just know that %%CAREGIVER%% still loves you
+
+# consolation~
+don't worry, it'll be alright
+it's okay to make mistakes
+%%CAREGIVER%% knows it's hard, but it will be okay
+
+# fallback~
+%%CAREGIVER%% is always here for you
+%%CAREGIVER%% is always here for you if you need %%THEM%%
+come here, sit on my lap while we figure this out together
+
+# encouragement~
+never give up, my love
+just a little further, %%CAREGIVER%% knows you can do it
+%%CAREGIVER%% knows you'll get there, don't worry about it
+
+# clean up~
+did %%CAREGIVER%%'s %%SWEETIE%% make a big mess
+"
+MOMMY_ENCOURAGEMENTS_EXTRA=""
+MOMMY_ENCOURAGEMENTS_ENABLED="1"
+
+MOMMY_FORBIDDEN_WORDS=""
+MOMMY_IGNORED_STATUSES="130"
